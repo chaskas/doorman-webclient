734dbddfda7def9980285d869ea1ac82e8c4f7c8
carlosriquelmegodoy <carlosriquelmegodoy@gmail.com>
2017-05-15 23:27:03 -0400

    member service

--- src/app/services/member.service.ts
+++ src/app/services/member.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Headers, Http, Response } from '@angular/http';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 
 import 'rxjs/add/operator/toPromise';
@@ -9,16 +9,26 @@ import { Member } from '../model/member';
 
 @Injectable()
 export class MemberService {
+
+  private url = this.config.get('host') + '/people';
+
+  private headers = new Headers({'Content-Type': 'application/json'});
+
   constructor(
     private http: Http,
     private config: AppConfig
 
   ) { }
 
+  createMembers(member_id: number, rut: string, first_name: string, last_name: string, gender: string, email: string, phone: string, mtype: number, last_seen:string, created_at:string, updated_at: string)
+	{
+    let url = this.config.get('host') + '/member';
+    let body = JSON.stringify({member_id: member_id, rut: rut, first_name: first_name, last_name: last_name,
+                              gender: gender, email: email, phone: phone, mtype: mtype, last_seen: last_seen,
+                            created_at: created_at, updated_at: updated_at});
+    let headers      = new Headers({ 'Content-Type': 'application/json' });
+    let options      = new RequestOptions({ headers: headers });
 
-private handleError(error: any): Promise<any> {
-  console.error('An error occurred', error);
-return Promise.reject(error.message || error);
-}
+    return this.http.post(url, body, options);
 
-}
+}}
