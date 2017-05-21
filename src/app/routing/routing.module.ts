ccce166d9fd99eee2209fd4953465c38e5cc2743
Rodrigo Campos <rodrigo@webdevel.cl>
2017-05-21 16:05:15 -0400

    menu updated

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -5,34 +5,15 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { DashboardComponent } from '../components/dashboard/dashboard.component';
 import { CalendarComponent } from '../components/calendar/calendar.component';
+import { MemberListComponent } from '../components/member/member-list/member-list.component';
 
 const routes: Routes = [
- // { path: '', component: LoginComponent },
- { path: 'dash', component: DashboardComponent ,
-    children:[
-      { path : '', component: CalendarComponent },
- //      { path : 'business/profile', component: BusinessProfileComponent },
- //      { path : 'business/offers', component: BusinessOffersListComponent },
- //      { path : 'business/offers/show/:id', component: BusinessOffersShowComponent },
- //      { path : 'business/offers/edit/:id', component: BusinessOffersEditComponent },
- //      { path : 'business/offers/create', component: BusinessOffersCreateComponent},
- //      { path : 'business/detail/:id', component: BusinessDetailComponent },
- //      { path : 'business/branches/create', component: BusinessBranchCreateComponent},
- //      { path : 'business/branches/edit/:id', component: BusinessBranchEditComponent},
- //      { path : 'business/branches', component: BusinessBranchListComponent},
- //      { path : 'business/history/:id', component: HistoryComponent},
- //      { path : 'offer', component: OfferComponent},
- //      { path : 'offer/detail/:id', component: OfferDetailComponent},
- //      { path : 'apply', component: ApplyComponent },
- //      { path : 'profile', component: ProfileComponent},
- //      { path : 'profile/avatar', component: UserProfileAvatarComponent}
-    ]
+ {  path: '', component: DashboardComponent ,
+      children:[
+        { path : '', component: CalendarComponent },
+        { path : 'members', component: MemberListComponent }
+      ]
  },
- // { path: 'menu', component: MenuComponent, canActivate: [Angular2TokenService] },
- // { path: 'header', component: HeaderComponent },
- // { path: 'signin', component: LoginComponent },
- // { path: 'signup', component: RegisterComponent },
- // { path: 'logout', component: LogoutComponent, canActivate: [Angular2TokenService] }
 ];
 
 @NgModule({
